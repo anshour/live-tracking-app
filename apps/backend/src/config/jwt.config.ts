60af54f1edb789c6750d4034bc4acb68dbc23287
Anshor J <anshorje@gmail.com>
2025-07-12 22:09:55 +0700

    Fixing invalid jwt config

--- apps/backend/src/config/jwt.config.ts
+++ apps/backend/src/config/jwt.config.ts
@@ -1,8 +1,11 @@
 import { registerAs } from '@nestjs/config';
+import { JwtModuleOptions } from '@nestjs/jwt';
 
-const jwtConfig = registerAs('jwt', () => ({
+const jwtConfig = registerAs<JwtModuleOptions>('jwt', () => ({
   secret: process.env.JWT_SECRET || 'defaultSecret',
-  expiresIn: process.env.JWT_EXPIRES_IN || '1h',
+  signOptions: {
+    expiresIn: process.env.JWT_EXPIRES_IN || '1h',
+  },
 }));
 
 //TODO: ADD CONFIG VALIDATION
