eb34ecbb6daac46809b39f8701555f7b76dc5a39
Anshor J <anshorje@gmail.com>
2025-07-13 20:43:54 +0700

    Add endpoint to fetch current user's tracker
    Introduced a new /me endpoint in TrackerController to allow users to fetch their own tracker. Also added TODO comments to restrict tracker visibility to subscribed users in future updates.

--- apps/backend/src/tracker/controllers/tracker.controller.ts
+++ apps/backend/src/tracker/controllers/tracker.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Request, UseGuards } from '@nestjs/common';
 import { TrackerService } from '../services/tracker.service';
 import { Tracker, TrackerHistory } from '@livetracking/shared';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
@@ -10,11 +10,19 @@ export class TrackerController {
 
   @Get()
   async getAllTrackers(): Promise<Tracker[]> {
+    // TODO: ONLY SHOW TRACKERS THAT THE USER IS SUBSCRIBED TO
     return this.trackerService.getAllTrackers();
+  }
+
+  @Get('/me')
+  async getMyTracker(@Request() req): Promise<Tracker | null> {
+    const userId = req.user.id as number;
+    return this.trackerService.findTrackerByUserId(userId);
   }
 
   @Get(':id/histories')
   async getTrackerHistory(@Param('id') id: string): Promise<TrackerHistory[]> {
+    // TODO: CAN ONLY SHOW TRACKERS THAT THE USER IS SUBSCRIBED TO
     return this.trackerService.getTrackerHistory(parseInt(id, 10));
   }
 }
